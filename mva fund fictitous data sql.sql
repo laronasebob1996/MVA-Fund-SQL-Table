6a4a94269ec80b3ccd9c959d7a1dfdda6d4e7b3a
Larona Ntungwana <117768478+laronasebob1996@users.noreply.github.com>
2023-04-09 11:33:29 +0200

    update mva fund fictitious data

--- mva fund fictitous data sql.sql
+++ mva fund fictitous data sql.sql
@@ -33,6 +33,143 @@
 
 
 -- SELECT Cause, AVG(Amount_Claimed) as Avg_Claimed, AVG(Amount_Paid) as Avg_Paid
+
+--UPDATE MVA_Claims
+--SET Amount_Owed = Amount_Claimed - COALESCE(Amount_Paid, 0);
+
+--Show the claims with the highest amount owed
+--SELECT *
+--FROM MVA_Claims
+--WHERE Status = 'Paid' AND Amount_Owed > 0
+--ORDER BY Amount_Owed DESC;
+
+--Find the claimant(s) with the highest amount claimed
+--SELECT Claimant_Name, MAX(Amount_Claimed) as Max_Claimed
+--FROM MVA_Claims
+--GROUP BY Claimant_Name
+--ORDER BY Max_Claimed DESC
+--LIMIT 1;
+
+--Find the average amount claimed per month
+--SELECT DATE_TRUNC('month', Date_of_Accident) as Month, AVG(Amount_Claimed) as Avg_Claimed
+--FROM MVA_Claims
+--GROUP BY Month
+--ORDER BY Month;
+
+--Find the percentage of claims that were denied
+--SELECT COUNT(CASE WHEN Status = 'Denied' THEN 1 END) * 100.0 / COUNT(*) as Denied_Percentage
+--FROM MVA_Claims;
+
+--Find the total amount claimed and paid by region and cause
+--SELECT Region, Cause, SUM(Amount_Claimed) as Total_Claimed, SUM(Amount_Paid) as Total_Paid
+--FROM MVA_Claims
+--GROUP BY Region, Cause
+--ORDER BY Region, Cause;
+
+--Find the number of claims that occurred in each year
+--SELECT EXTRACT(YEAR FROM Date_of_Accident) as Year, COUNT(*) as Num_Claims
+--FROM MVA_Claims
+--GROUP BY Year
+--ORDER BY Year;
+
+--Find the top 5 claimants with the highest amount paid
+--SELECT Claimant_Name, SUM(Amount_Paid) as Total_Paid
+--FROM MVA_Claims
+--WHERE Status = 'Paid'
+--GROUP BY Claimant_Name
+--ORDER BY Total_Paid DESC
+--LIMIT 5;
+
+--Find the average amount claimed and paid by cause and region for only the approved claims
+--SELECT Cause, Region, AVG(Amount_Claimed) as Avg_Claimed, AVG(Amount_Paid) as Avg_Paid
+--FROM MVA_Claims
+--WHERE Status = 'Approved'
+--GROUP BY Cause, Region
+--ORDER BY Cause, Region;
+
+--Find the claims with a reported accident date in the last 30 days
+--SELECT *
+--FROM MVA_Claims
+--WHERE Date_of_Accident >= DATEADD(day, -30, GETDATE());
+
+--Delete all claims with a status of "Denied"
+--DELETE FROM MVA_Claims
+--WHERE Status = 'Denied';
+
+
+
+
+laronasebobi@gmail.com
+write unique  sql code from the one written above
+--Add a new column to MVA_Claims table to calculate the ratio of amount paid to amount claimed
+--ALTER TABLE MVA_Claims
+
+--ADD Payment_Ratio FLOAT;
+--UPDATE MVA_Claims
+--SET Payment_Ratio = COALESCE(Amount_Paid, 0) / Amount_Claimed;
+
+--Show the claims where the payment ratio is less than 0.5
+--SELECT *
+--FROM MVA_Claims
+--WHERE Payment_Ratio < 0.5;
+
+--Find the top 3 regions with the highest average amount claimed per claimant
+--SELECT Region, AVG(Amount_Claimed) as Avg_Claimed
+--FROM MVA_Claims
+--GROUP BY Region
+--HAVING COUNT(DISTINCT Claimant_Name) >= 3
+--ORDER BY Avg_Claimed DESC
+--LIMIT 3;
+
+--Find the total number of claims, total amount claimed, and total amount paid by cause
+--SELECT Cause, COUNT(*) as Num_Claims, SUM(Amount_Claimed) as Total_Claimed, SUM(Amount_Paid) as Total_Paid
+--FROM MVA_Claims
+--GROUP BY Cause
+--ORDER BY Num_Claims DESC;
+
+--Find the claims where the difference between the date of accident and the date of report is greater than 7 days
+--SELECT *
+--FROM MVA_Claims
+--WHERE DATEDIFF(day, Date_of_Accident, Date_of_Report) > 7;
+
+--Find the number of claims that are pending or denied by region and cause
+--SELECT Region, Cause, COUNT(*) as Num_Claims
+--FROM MVA_Claims
+--WHERE Status = 'Pending' OR Status = 'Denied'
+--GROUP BY Region, Cause
+--ORDER BY Num_Claims DESC;
+
+--Find the claims where the cause is "Collision" and the region is not "Gaborone"
+--SELECT *
+--FROM MVA_Claims
+--WHERE Cause = 'Collision' AND Region != 'Gaborone';
+
+--Find the average amount paid per claimant for the top 5 claimants with the highest amount claimed
+--SELECT Claimant_Name, AVG(Amount_Paid) as Avg_Paid
+--FROM MVA_Claims
+--WHERE Claimant_Name IN (
+-- SELECT Claimant_Name
+-- FROM MVA_Claims
+-- ORDER BY Amount_Claimed DESC
+-- LIMIT 5
+--)
+--GROUP BY Claimant_Name
+--ORDER BY Avg_Paid DESC;
+
+--Find the claims where the amount claimed is less than the average amount claimed for the cause and region
+--SELECT *
+--FROM MVA_Claims c
+--WHERE Amount_Claimed < (
+-- SELECT AVG(Amount_Claimed)
+-- FROM MVA_Claims
+-- WHERE Cause = c.Cause AND Region = c.Region
+--);
+
+
+
+
+
+
 --FROM MVA_Claims
 --GROUP BY Cause
 
